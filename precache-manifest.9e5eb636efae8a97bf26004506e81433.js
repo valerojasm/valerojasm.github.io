4958b3d08f314e9810fccd7e771632e678ac6bc2
Valentina Rojas <vrojas5@uc.cl>
2020-06-07 23:49:11 -0400

    Updates

---
+++ precache-manifest.9e5eb636efae8a97bf26004506e81433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf834be5ed0d7357baa3da7616dc366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "544725cc97db0b667662",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc7736abea9040b8e3c9",
+    "url": "/static/js/2.0a6b17ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a6b17ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544725cc97db0b667662",
+    "url": "/static/js/main.8c3ae3bb.chunk.js"
+  },
+  {
+    "revision": "d4ec3323ad307e4a4ee7",
+    "url": "/static/js/runtime-main.e98a66c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
